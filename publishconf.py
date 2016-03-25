29124a111b0c8b00d10f57a2c6e54e68759b155f
Achilleas Pipinellis <axilleas@axilleas.me>
2016-03-25 12:34:08 +0200

    Output dir is imported from pelicanconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,14 +10,13 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://pages.gitlab.io'
+SITEURL = 'https://pages.gitlab.io/pelican'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
-OUTPUT_PATH = 'public'
 
 # Following items are often useful when publishing
 
