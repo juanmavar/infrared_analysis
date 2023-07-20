ce19430ab16acea66993f584e08bb0b7027366a6
Juan Manuel Varela Perez <juan.manuel.varela@fing.edu.uy>
2023-07-20 20:39:23 -0300

    Update pelicanconf.py

--- page_conf/pelicanconf.py
+++ page_conf/pelicanconf.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'Juan Manuel Varela'
-SITENAME = 'Tratamiento de imágenes 2023 - Proyecto final'
+AUTHOR = "Juan Manuel Varela"
+SITENAME = "Tratamiento de imágenes 2023 - Proyecto final"
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'America/Montevideo'
+TIMEZONE = "America/Montevideo"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -18,8 +18,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Facultad de Ingeniería', 'https://www.fing.edu.uy/'),
-         ('Instituto de Ingeniería Eléctrica', 'https://iie.fing.edu.uy/'),)
+LINKS = (
+    ("Facultad de Ingeniería", "https://www.fing.edu.uy/"),
+    ("Instituto de Ingeniería Eléctrica", "https://iie.fing.edu.uy/"),
+)
 
 # # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
@@ -32,22 +34,22 @@ DEFAULT_PAGINATION = 10
 
 # PLUGINS--------------------------------
 # Where to look for plugins
-PLUGIN_PATHS = ['pelican-plugins']  #['../pelican-plugins']
+PLUGIN_PATHS = ["pelican-plugins"]  # ['../pelican-plugins']
 # Which plugins to enable
 # PLUGIN PARA PONER NUMERO A LAS FIGURAS ->  figure-ref
-#https://github.com/cmacmackin/figure-ref/tree/40e04d32bff468a6b3e63c373c5d95fca39783fe
+# https://github.com/cmacmackin/figure-ref/tree/40e04d32bff468a6b3e63c373c5d95fca39783fe
 # PLUGIN PARA CITAS ->  pelican-cite
-#https://github.com/VorpalBlade/pelican-cite
-PLUGINS = ['figure-ref', 'pelican-cite']
+# https://github.com/VorpalBlade/pelican-cite
+PLUGINS = ["figure-ref", "pelican-cite"]
 
 
 
 # ORDENAR LAS PAGINAS EN EL MENU POR EL ATRUBUTO "Ordinal" -------
 # si no, te quedan ordenadas alfabéticamente
-PAGE_ORDER_BY = 'ordinal'
+PAGE_ORDER_BY = "ordinal"
 
 # UBICACION DE bibtex que usa el plugin pelican-cite
-PUBLICATIONS_SRC = 'content/biblio.bib'
+PUBLICATIONS_SRC = "content/biblio.bib"
 
 
 # CAMBIAR EL ASPECTO
